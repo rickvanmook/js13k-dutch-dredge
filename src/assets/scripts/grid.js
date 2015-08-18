6c74a4eda22ad248c0a99be30ae35b1809e1721d
Rick van Mook <rick@rvmook.com>
2015-08-19 00:09:35 +0200

    Added simple, somewhat dynamic, cell element generation

--- src/assets/scripts/grid.js
+++ src/assets/scripts/grid.js
@@ -1,3 +1,36 @@
+var doc = document,
+	appendChild = 'appendChild',
+	wrap = doc.body[appendChild](createDiv('wrap')),
+	cells = [],
+	i = 1;
+
+
+for(i; i <= 2;i++) {
+
+	cells.push(createCell('cube-' + i));
+}
+
+function createCell(cubeName) {
+
+	var sides = ['front','back','top','bottom','left','right'],
+		container = wrap[appendChild](createDiv('cube ' + cubeName));
+
+	while(sides.length) {
+		container[appendChild](createDiv(sides.shift()));
+	}
+
+	return container;
+}
+
 /**
- * Created by rick on 18/08/15.
+ *
+ * @param className
+ * @returns {Element}
  */
+function createDiv(className) {
+
+	var div = doc.createElement('div');
+	div.className = className;
+
+	return div;
+}
