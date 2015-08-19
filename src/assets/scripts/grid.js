a48e6980dfa0236b34c1434c8601b920b9cd6259
Rick van Mook <rick@rvmook.com>
2015-08-20 01:42:39 +0200

    Added somewhat minified way for cell selection

--- src/assets/scripts/grid.js
+++ src/assets/scripts/grid.js
@@ -1,8 +1,9 @@
 var doc = document,
 	TRANSFORM = 'rotateX(-25deg) rotateY(-45deg) translate3d(Apx,Bpx,0)',
-	appendChild = 'appendChild',
+	ACTIVE_CLASS = appendChild = 'appendChild',
 	wrap = doc.body[appendChild](createDiv('wrap')),
-	cells = [],
+	hoverCss='',
+	cells = {},
 	x = 0,
 	y,
 	OFFSET_X = 0,
@@ -11,41 +12,80 @@ var doc = document,
 	COLUMNS = 6;
 
 
-
-
-
 for(x; x < ROWS; x++) {
 
 	for(y = 0; y < COLUMNS; y++) {
 
-		cells.push(createCell(x,y));
+		cells[ACTIVE_CLASS+x+y] = createCell(x,y);
 	}
 }
+
+var styleEl = doc.getElementsByTagName('style')[0];
+styleEl.innerHTML = hoverCss;
 
 function createCell(x,y) {
 
 	var sides = ['front','back','top','bottom','left','right'],
-		container = wrap[appendChild](createDiv('cube cube-'+x+y));
+		className = ACTIVE_CLASS+x+y,
+		container = wrap[appendChild](createDiv('cube ' + className));
 
 	container.x = x;
 	container.y = y;
 
-	var _x = x*59.5 - y*59;
-	var _y = y*39;
 
-	var transform = TRANSFORM.replace('A',_x).replace('B',_y);
-	console.log('x: ' + x + ', y: ' + y, transform);
-	container.style.transform = transform;
+	hoverCss+= '.' + className + '{transform:'+positionCell(container)+'}';
+	hoverCss+= '.' + className + '.'+ACTIVE_CLASS+'{transform:'+positionCell(container,1)+'}';
 
 	while(sides.length) {
 		container[appendChild](createDiv(sides.shift()));
 	}
 
+	container.addEventListener('mouseover',activateCells);
+	container.addEventListener('mouseout', deactivateCells);
+
 	return container;
 }
 
+function activateCells() {
+	cellsAction(this, 'add');
+}
+
+function deactivateCells() {
+	cellsAction(this, 'remove');
+}
+
+function cellsAction(cell, method) {
+
+	getCell(cell.x,cell.y);
+	getCell(cell.x-1,cell.y);
+	getCell(cell.x+1,cell.y);
+	getCell(cell.x,cell.y-1);
+	getCell(cell.x,cell.y+1);
+
+	function getCell(x,y){
+		var actualCell = cells[ACTIVE_CLASS+x+y];
+		if(actualCell && actualCell.b != method) {
+			actualCell.b=method;
+			actualCell.classList[method](ACTIVE_CLASS);
+		}
+	}
+}
+
+
+function positionCell(cell, isActive, isRotated) {
+
+	var _x = cell.x*59.5 - cell.y*59;
+	var _y = cell.y*39;
+
+	if(isActive) {
+
+		_y-=15;
+	}
+
+	return TRANSFORM.replace('A',_x).replace('B',_y);
+}
+
 /**
- *
  * @param className
  * @returns {Element}
  */
