b71ffdbe15a9768d6d1460a8b939bf62a934230d
Rick van Mook <rick@rvmook.com>
2015-08-20 00:14:46 +0200

    Added dynamic transform based grid

--- src/assets/scripts/grid.js
+++ src/assets/scripts/grid.js
@@ -1,19 +1,41 @@
 var doc = document,
+	TRANSFORM = 'rotateX(-25deg) rotateY(-45deg) translate3d(Apx,Bpx,0)',
 	appendChild = 'appendChild',
 	wrap = doc.body[appendChild](createDiv('wrap')),
 	cells = [],
-	i = 1;
+	x = 0,
+	y,
+	OFFSET_X = 0,
+	OFFSET_Y = 0,
+	ROWS = 6,
+	COLUMNS = 6;
 
 
-for(i; i <= 2;i++) {
 
-	cells.push(createCell('cube-' + i));
+
+
+for(x; x < ROWS; x++) {
+
+	for(y = 0; y < COLUMNS; y++) {
+
+		cells.push(createCell(x,y));
+	}
 }
 
-function createCell(cubeName) {
+function createCell(x,y) {
 
 	var sides = ['front','back','top','bottom','left','right'],
-		container = wrap[appendChild](createDiv('cube ' + cubeName));
+		container = wrap[appendChild](createDiv('cube cube-'+x+y));
+
+	container.x = x;
+	container.y = y;
+
+	var _x = x*59.5 - y*59;
+	var _y = y*39;
+
+	var transform = TRANSFORM.replace('A',_x).replace('B',_y);
+	console.log('x: ' + x + ', y: ' + y, transform);
+	container.style.transform = transform;
 
 	while(sides.length) {
 		container[appendChild](createDiv(sides.shift()));
